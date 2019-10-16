f41e412256474b6d334d7fc52fae293208e23a89
dizturb <lucasbaken@gmail.com>
2019-10-16 12:31:56 +0200

    Changed project architecture

--- client/src/app/components/map-view/map-view.component.ts
+++ client/src/app/components/map-view/map-view.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import WMTS from 'ol/source/WMTS';
+import Projection from 'ol/proj/Projection';
+import { getTopLeft } from 'ol/extent';
+import WMTSTileGrid from 'ol/tilegrid/WMTS';
+import { Tile } from 'openlayers';
 
 @Component({
   selector: 'app-map-view',
@@ -13,6 +17,35 @@ export class MapViewComponent implements OnInit {
 
   private map: Map;
 
+  private projectionExtent = [-285401.92, 22598.08, 595401.92, 903401.92];
+  private projection = new Projection({ code: 'EPSG:28992', units: 'm', extent: this.projectionExtent });
+
+  private resolutions = [
+    3440.640,
+    1720.320,
+    860.160,
+    430.080,
+    215.040,
+    107.520,
+    53.750,
+    26.880,
+    13.440,
+    6.720,
+    3.360,
+    1.680,
+    0.840,
+    0.420,
+    0.210
+  ];
+  private matrixIds = new Array(15);
+
+  private layers = {
+    brt: 'brtachtergrondkaart',
+    brtGrijs: 'brtachtergrondkaartgrijs',
+    brtPastel: 'brtachtergrondkaartpastel',
+    brtWater: 'brtachtergrondkaartwater',
+  }
+
   constructor() { }
 
   ngOnInit() {
@@ -20,7 +53,41 @@ export class MapViewComponent implements OnInit {
   }
 
   private initializeMap() {
-    this.map = new Map({ target: 'map' });
+
+    for (let i = 0; i < this.matrixIds.length; i++) {
+      this.matrixIds[i] = 'EPSG:28992:' + i;
+    }
+
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          opacity: 0.7,
+          source: new WMTS({
+            attributions: 'Kaartgegevens: $copy <a href="http://www.kadaster.nl>Kadaster</a>',
+            url: 'https://geodata.nationaalgeoregister.nl/tiles/service/wmts',
+            layer: this.layers.brtWater,
+            matrixSet: 'EPSG:28992',
+            format: 'image/png',
+            projection: this.projection,
+            tileGrid: new WMTSTileGrid({
+              origin: getTopLeft(this.projectionExtent),
+              resolutions: this.resolutions,
+              matrixIds: this.matrixIds
+            }),
+            style: 'default',
+            wrapX: false
+          })
+        })
+      ],
+      view: new View({
+        center: [150000, 450000],
+        projection: this.projection,
+        zoom: 3,
+        minZoom: 0,
+        maxZoom: 15
+      })
+    });
   }
 
 }
--- server/Startup.cs
+++ server/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,9 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-namespace GeoLocator
+namespace server
 {
     public class Startup
     {
@@ -19,21 +18,18 @@ namespace GeoLocator
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+
+            app.Run(async (context) =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
