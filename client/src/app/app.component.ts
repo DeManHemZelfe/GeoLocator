2b5841261774a0077e1dd6eaac6959724424c3f0
Lucas Peelen <31964328+Dizturb@users.noreply.github.com>
2019-10-31 09:56:29 +0100

    Revert "Exp map"

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,11 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   title = 'client';
-
-  onButtonClick() {
-    console.log('click');
-  }
 }
